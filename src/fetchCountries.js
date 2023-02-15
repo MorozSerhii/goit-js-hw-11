055f3f17bcbddee215c2c3d3092bd0aeb375a44f
Serhii <ecprap@gmail.com>
2023-02-15 22:09:03 +0200

    1

--- src/fetchCountries.js
+++
@@ -1,5 +0,0 @@
-export default async function fetchCountries(name) {
-  let url = `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`;
-
-  return fetch(url).then(res => res.json());
-}
