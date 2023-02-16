4cbc431405b469eb46ffbe39104cfaa523187f9c
Serhii <ecprap@gmail.com>
2023-02-16 02:42:02 +0200

    1

---
+++ src/API.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const url = `https://pixabay.com/api/`;
+
+function getImages(searchQuery) {
+  return axios.get(`${url}`, {
+    params: {
+      key: '33676510-60d9800a173eb3eec07b521d4',
+      q: searchQuery,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      per_page: 5,
+    },
+  });
+}
+
+export default getImages;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,60 @@
 import './css/styles.css';
+import getImages from './API';
+
+const inputElement = document.querySelector('input');
+const imageContainer = document.querySelector('.gallery');
+const form = document.querySelector('.search-form');
+
+inputElement.addEventListener('input', reqvestData);
+let allImages = [];
+
+function reqvestData() {
+  let search = inputElement.value;
+
+  if (search === '') {
+    return;
+  }
+
+  getImages(search).then(response => {
+    let searchQuery = response.data.hits;
+    console.log(searchQuery);
+
+    return (allImages = searchQuery.reduce(
+      (markup, searchQuery) => createMarkup(searchQuery) + markup,
+      ''
+    ));
+  });
+}
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  imageContainer.innerHTML = allImages;
+});
+
+function createMarkup({
+  webformatURL,
+  largeImageURL,
+  tags,
+  likes,
+  views,
+  comments,
+  downloads,
+}) {
+  return `<div class="photo-card">
+  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes: ${likes}</b>
+    </p>
+    <p class="info-item">
+      <b>Views: ${views}</b>
+    </p>
+    <p class="info-item">
+      <b>Comments: ${comments}</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads: ${downloads}</b>
+    </p>
+  </div>
+</div>`;
+}
